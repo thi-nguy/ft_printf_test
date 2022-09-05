d105392a1e3694d1d8f61934c7dfaeca2711bc85
Thi hong nhung NGUYEN <thi-nguy@e1r2p14.clusters.42paris.fr>
2022-09-05 10:22:29 +0200

    Change main

--- main.c
+++ main.c
@@ -5,46 +5,46 @@ void c(void)
 	printf("--------------------%%c--------------------\n");		//Perfect
 	printf("01) Vrai PRINTF : |%c|\n", 'a');
 	ft_printf("01) Mon PRINTF  : |%c|\n", 'a');
-	printf("02) Vrai PRINTF : |%c|\n", 'z');
-	ft_printf("02) Mon PRINTF  : |%c|\n", 'z');
-	printf("03) Vrai PRINTF : |%c|\n", '!');
-	ft_printf("03) Mon PRINTF  : |%c|\n", '!');
-	printf("04) Vrai PRINTF : |%c|\n", 'R');
-	ft_printf("04) Mon PRINTF  : |%c|\n", 'R');
-	printf("05) Vrai PRINTF : |%c|\n", 'R');
-	ft_printf("05) Mon PRINTF  : |%c|\n", 'R');
-	printf("06) Vrai PRINTF : |%c|\n", '3');
-	ft_printf("06) Mon PRINTF  : |%c|\n", '3');
-	printf("07) Vrai PRINTF : |%c|\n", '\0');
-	ft_printf("07) Mon PRINTF  : |%c|\n", '\0');
+	printf("02) Vrai PRINTF : |%cp|\n", 'z');
+	ft_printf("02) Mon PRINTF  : |%cp|\n", 'z');
+	printf("03) Vrai PRINTF : |%ci|\n", '!');
+	ft_printf("03) Mon PRINTF  : |%ci|\n", '!');
+	printf("04) Vrai PRINTF : |%cs|\n", 'R');
+	ft_printf("04) Mon PRINTF  : |%cs|\n", 'R');
+	printf("05) Vrai PRINTF : |%cx|\n", 'R');
+	ft_printf("05) Mon PRINTF  : |%cx|\n", 'R');
+	printf("06) Vrai PRINTF : |%cX|\n", '3');
+	ft_printf("06) Mon PRINTF  : |%cX|\n", '3');
+	printf("07) Vrai PRINTF : |%cd|\n", '\0');
+	ft_printf("07) Mon PRINTF  : |%cd|\n", '\0');
 	printf("08) Vrai PRINTF : |%c|\n", '\0');
 	ft_printf("08) Mon PRINTF  : |%c|\n", '\0');
-	printf("09) Vrai PRINTF : |%5c|\n", '\0');
-	ft_printf("09) Mon PRINTF  : |%5c|\n", '\0');
+	printf("09) Vrai PRINTF : |%c|\n", '\0');
+	ft_printf("09) Mon PRINTF  : |%c|\n", '\0');
 	printf("10) Vrai PRINTF : |%c|\n", NULL);
 	ft_printf("10} Mon PRINTF  : |%c|\n", NULL);
-	printf("11) Vrai PRINTF : |%0cw|\n", 'a');
-	ft_printf("11} Mon PRINTF  : |%0cw|\n", 'a');
+	printf("11) Vrai PRINTF : |%cw|\n", 'a');
+	ft_printf("11} Mon PRINTF  : |%cw|\n", 'a');
 }
 void s(void)
 {
 	ft_printf("--------------------%%s--------------------\n");	//Perfect
 	printf("01) Vrai PRINTF : |%s|\n", "salut du monde!!@#$%%");
 	ft_printf("01) Mon PRINTF  : |%s|\n", "salut du monde!!@#$%%");
-	printf("02) Vrai PRINTF : |%s|\n", "test");
-	ft_printf("02) Mon PRINTF  : |%s|\n", "test");
-	printf("03) Vrai PRINTF : |%s|\n", "test2");
-	ft_printf("03) Mon PRINTF  : |%s|\n", "test2");
-	printf("04) Vrai PRINTF : |%s|\n", "42");
-	ft_printf("04) Mon PRINTF  : |%s|\n", "42");
-	printf("05) Vrai PRINTF : |%s|\n", "42");
-	ft_printf("05) Mon PRINTF  : |%s|\n", "42");
-	printf("06) Vrai PRINTF : |%s|\n", "string");
-	ft_printf("06) Mon PRINTF  : |%s|\n", "string");
-	printf("07) Vrai PRINTF : |%s|\n", "string");
-	ft_printf("07) Mon PRINTF  : |%s|\n", "string");
-	printf("08) Vrai PRINTF : |%s|\n", "precision");
-	ft_printf("08) Mon PRINTF  : |%s|\n", "precision");
+	printf("02) Vrai PRINTF : |%sc|\n", "test");
+	ft_printf("02) Mon PRINTF  : |%sc|\n", "test");
+	printf("03) Vrai PRINTF : |%si|\n", "test2");
+	ft_printf("03) Mon PRINTF  : |%si|\n", "test2");
+	printf("04) Vrai PRINTF : |%sd|\n", "42");
+	ft_printf("04) Mon PRINTF  : |%sd|\n", "42");
+	printf("05) Vrai PRINTF : |%sp|\n", "42");
+	ft_printf("05) Mon PRINTF  : |%sp|\n", "42");
+	printf("06) Vrai PRINTF : |%sx|\n", "string");
+	ft_printf("06) Mon PRINTF  : |%sx|\n", "string");
+	printf("07) Vrai PRINTF : |%sX|\n", "string");
+	ft_printf("07) Mon PRINTF  : |%sX|\n", "string");
+	printf("08) Vrai PRINTF : |%su|\n", "precision");
+	ft_printf("08) Mon PRINTF  : |%su|\n", "precision");
 	printf("09) Vrai PRINTF : |%s|\n", "precision");
 	ft_printf("09) Mon PRINTF  : |%s|\n", "precision");
 	printf("10) Vrai PRINTF : |%s|\n", "precision");
@@ -79,21 +79,18 @@ void num_u(void)
 {
 	
 	ft_printf("--------------------%%u--------------------\n");
-	printf("01) Vrai PRINTF: |%u|\n", 420);
-	ft_printf("01) Mon PRINTF : |%u|\n", 420);
+	printf("01) Vrai PRINTF: |%ui|\n", 420);
+	ft_printf("01) Mon PRINTF : |%ui|\n", 420);
 	printf("02) Vrai PRINTF: |%u|\n", -420);
 	ft_printf("02) Mon PRINTF : |%u|\n", -420);
-	//neu co precision thi se in number va so 0 cho du precision, 
- 	//neu co them width thi se ko in gi cho du width
-	// thu truong hop precision nho hon do dai cua so
-	printf("19) Vrai PRINTF : |%u|\n", 0);
-	ft_printf("19) Mon PRINTF :  |%u|\n", 0);
-	printf("20) Vrai PRINTF : |%u|\n", -1234567);
-	ft_printf("20) Mon PRINTF :  |%u|\n", -1234567);
-	printf("21) Vrai PRINTF : |%u|\n", 123456798237472);
-	ft_printf("21) Mon PRINTF :  |%u|\n", 123456798237472);
-	printf("21) Vrai PRINTF : |%u|\n", -123456798237472);
-	ft_printf("21) Mon PRINTF :  |%u|\n", -123456798237472);
+	printf("19) Vrai PRINTF : |%ud|\n", 0);
+	ft_printf("19) Mon PRINTF :  |%ud|\n", 0);
+	printf("20) Vrai PRINTF : |%up|\n", -1234567);
+	ft_printf("20) Mon PRINTF :  |%up|\n", -1234567);
+	printf("21) Vrai PRINTF : |%uc|\n", 123456798237472);
+	ft_printf("21) Mon PRINTF :  |%uc|\n", 123456798237472);
+	printf("21) Vrai PRINTF : |%us|\n", -123456798237472);
+	ft_printf("21) Mon PRINTF :  |%us|\n", -123456798237472);
 
 }
 
@@ -212,12 +209,12 @@ void p(void)
 void other_cases(void)
 {
 	ft_printf("--------------------%%Other Case--------------------\n");
-	printf("01) Vrai PRINTF : |%%%|\n");
-	ft_printf("01) Mon PRINTF  : |%%%|\n");
-	printf("02) Vrai PRINTF : |%%|\n");
-	ft_printf("02) Mon PRINTF  : |%%|\n");
-	printf("03) Vrai PRINTF : |%%%%|\n");
-	ft_printf("03) Mon PRINTF  : |%%%%|\n");
+	printf("01) Vrai PRINTF : |%%|\n");
+	ft_printf("01) Mon PRINTF  : |%%|\n");
+	printf("02) Vrai PRINTF : |%%%%|\n");
+	ft_printf("02) Mon PRINTF  : |%%%%|\n");
+	printf("03) Vrai PRINTF : |%%%%%%|\n");
+	ft_printf("03) Mon PRINTF  : |%%%%%%|\n");
 }
 
 int		main(void)
